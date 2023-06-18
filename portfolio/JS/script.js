896fc3ee7b6b91b6c0cb6aa16d0f22ac7ce0971b
Sasikanth <66306004+Balasasikanthvaddiparthi@users.noreply.github.com>
2023-06-18 09:13:53 +0530

    Add files via upload

---
+++ portfolio/JS/script.js
@@ -0,0 +1,24 @@
+function southernLinc() {
+    window.location.href = 'southernLinc.html';
+  }
+function vadaphone(){
+    window.location.href = 'vadaphone.html';
+}
+function webex(){
+    window.location.href = 'webex.html';
+}
+function viva(){
+    window.location.href = 'viva.html';
+}
+function sirius(){
+    window.location.href = 'sirius.html';
+}
+function verizon(){
+    window.location.href = 'verizon.html';
+}
+function dhiraagu(){
+    window.location.href = 'dhiraagu.html';
+}
+function minacs(){
+    window.location.href = 'minacs.html';
+}
